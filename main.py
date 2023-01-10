1e85a178a9efe42316a3c8e0b975725dcedd9f53
uwanyirigira <uwmarius6@gmail.com>
2023-01-10 12:03:16 +0200

    add about

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/about")
+def  read_root():
+    return {"this is about pages"}
+# this is backend
